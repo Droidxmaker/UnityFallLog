b0ffe30163370dde501cab02d03d63061052f21e
Droidxmaker <jokern123@gmail.com>
2015-01-24 04:19:25 +0100

    Fall througth.
    This is an log script (c#) for unity3d where the object (Box) falls
    throuthg something, not colliding

---
+++ BoxFallThrougth.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Security.Cryptography.X509Certificates;
+
+public class BoxFallThrougth : MonoBehaviour {
+
+    public class Trigger : MonoBehaviour { 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        }
+
+        void OnTrigger(Collider myTrigger)
+        {
+            var currentPossition = transform.position;
+
+            if (myTrigger.gameObject.name == "Box")
+            {
+                Debug.Log("Box went throught" + currentPossition); 
+            }
+        }
+
+	}
+}
